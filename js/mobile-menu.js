da84fbe744238fce1c495e8a1ec05d5d7203fa5d
nina-strizhachenko <nina.strizhachenko@gmail.com>
2021-12-30 00:30:22 +0200

    dz-08-part-1

---
+++ js/mobile-menu.js
@@ -0,0 +1,25 @@
+(() => {
+  const mobileMenu = document.querySelector('.data-menu-container');
+  const openMenuBtn = document.querySelector('.data-mobile-btn');
+
+  const toggleMenu = () => {
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('mobile-menu--is-open');
+    openMenu.classList.toggle('menu-btn--is-open');
+
+    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
