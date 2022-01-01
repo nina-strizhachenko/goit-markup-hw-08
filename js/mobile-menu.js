0d93727bfe824974d6f6f9d815e2adcc326121d7
nina-strizhachenko <nina.strizhachenko@gmail.com>
2022-01-01 16:10:11 +0200

    dz-08-part-2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,19 @@
 (() => {
-  const mobileMenu = document.querySelector('.data-menu-container');
-  const openMenuBtn = document.querySelector('.data-mobile-btn');
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
     const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('mobile-menu--is-open');
-    openMenu.classList.toggle('menu-btn--is-open');
+    mobileMenu.classList.toggle('is-open');
 
     const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
